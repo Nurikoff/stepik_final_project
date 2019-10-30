bd0eb91d69f4864dc9abc312abb9fff1c302648d
anton-m <nurikoff@mail.ru>
2019-10-30 15:25:45 +0200

    base and main page add

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,10 @@
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
+def go_to_login_page(browser):
+    link = browser.find_element_by_css_selector("#login_link")
+    link.click()
+
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+   browser.get(link)
+   go_to_login_page(browser)

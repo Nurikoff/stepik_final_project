0ebea0e192c7c425d9da0cb199eee4ced7c9e5d4
anton-m <nurikoff@mail.ru>
2019-10-31 11:17:46 +0200

    login page tested second type

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
@@ -8,7 +9,9 @@ def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()    # выполняем метод страницы - переходим на страницу логина
 
 def test_should_be_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
